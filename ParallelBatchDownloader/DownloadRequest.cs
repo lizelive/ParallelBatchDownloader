05590e6d72e3cc92d1395965503d2ea5e81affe1
lizelive <github@lize.live>
2021-05-15 15:37:19 -0700

    moved to own file

---
+++ ParallelBatchDownloader/DownloadRequest.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ParallelBatchDownloader
+{
+    record DownloadRequest(string Uri, string Path)
+    {
+        static readonly Uri Base = new("file:\\D:\\");
+        public static DownloadRequest Parse(string line)
+        {
+            var parts = line.Trim().Split('\t');
+            var u1 = "./"+new Uri(parts[1]).AbsolutePath.Substring(3);
+            return new DownloadRequest(parts[0], u1);
+        }
+
+        public override string ToString()
+        {
+            return $"{Uri}\t{Path}";
+        }
+    }
+}
--- ParallelBatchDownloader/Program.cs
+++ ParallelBatchDownloader/Program.cs
@@ -10,21 +10,6 @@ using System.Security.Cryptography;
 
 namespace ParallelBatchDownloader
 {
-    record DownloadRequest(string Uri, string Path)
-    {
-        static readonly Uri Base = new("file:\\D:\\");
-        public static DownloadRequest Parse(string line)
-        {
-            var parts = line.Trim().Split('\t');
-            var u1 = "./"+new Uri(parts[1]).AbsolutePath.Substring(3);
-            return new DownloadRequest(parts[0], u1);
-        }
-
-        public override string ToString()
-        {
-            return $"{Uri}\t{Path}";
-        }
-    }
     class Program
     {
         static object failock = new object();
