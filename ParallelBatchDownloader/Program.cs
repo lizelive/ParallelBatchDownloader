7ad98c1145c982737faa7b0a40363ca1cb660a2a
lizelive <github@lize.live>
2021-05-15 15:09:37 -0700

    Add project files.

---
+++ ParallelBatchDownloader/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Collections.Concurrent;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Security.Cryptography;
+
+namespace ParallelBatchDownloader
+{
+    record DownloadRequest(string Uri, string Path)
+    {
+        static readonly Uri Base = new("file:\\D:\\");
+        public static DownloadRequest Parse(string line)
+        {
+            var parts = line.Trim().Split('\t');
+            var u1 = "./"+new Uri(parts[1]).AbsolutePath.Substring(3);
+            return new DownloadRequest(parts[0], u1);
+        }
+
+        public override string ToString()
+        {
+            return $"{Uri}\t{Path}";
+        }
+    }
+    class Program
+    {
+        static object failock = new object();
+        static object winlock = new object();
+
+        static async Task DoSomething(string stuff)
+        {
+            var dr = DownloadRequest.Parse(stuff);
+            if (File.Exists(dr.Path))
+            {
+                return;
+            }
+            Directory.CreateDirectory(Path.GetDirectoryName(dr.Path));
+            //using WebClient client = new();
+
+            try
+            {
+                using HttpClient client = new();
+                using SHA256 mySHA256 = SHA256.Create();
+                Stopwatch timer = new();
+                timer.Start();
+                var data = await client.GetByteArrayAsync(dr.Uri);
+                var writeTask = File.WriteAllBytesAsync(dr.Path, data);
+                var hash = mySHA256.ComputeHash(data);
+                await writeTask;
+                timer.Stop();
+                lock (winlock)
+                {
+                    File.AppendAllTextAsync("D:\\downloaded.tsv", $"{Convert.ToHexString(hash)}\t{timer.ElapsedMilliseconds}\t{data.Length}\t{dr.Uri}\n");
+                }
+            }
+            catch (Exception e)
+            {
+                lock (failock)
+                {
+                    File.AppendAllText("D:\\failed.tsv", dr.ToString() + '\n');
+                }
+                Console.Error.WriteLine(e);
+                if(File.Exists(dr.Path))
+                    File.Delete(dr.Path);
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Directory.SetCurrentDirectory("D:\\");
+            Stopwatch timer = new();
+            timer.Start();
+            var toDownload = Utils.ReadLinesAsync("./todownload.tsv").AsyncParallelForEach(DoSomething, maxDegreeOfParallelism: 30);
+            toDownload.Wait();
+            timer.Stop();
+
+            Console.WriteLine(timer.ElapsedMilliseconds);
+        }
+    }
+}
---
+++ ParallelBatchDownloader/Utils.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading.Tasks;
+using System.Threading.Tasks.Dataflow;
+
+namespace ParallelBatchDownloader
+{
+    static class Utils
+    {
+        public static async IAsyncEnumerable<string> ReadLinesAsync(string path)
+        {
+            using (var file = File.OpenText(path))
+            {
+                string line;
+                while ((line = await file.ReadLineAsync())!= null)
+                {
+                    yield return line;
+                }
+            }
+        }
+
+
+        public static async Task AsyncParallelForEach<T>(this IAsyncEnumerable<T> source, Func<T, Task> body, int maxDegreeOfParallelism = DataflowBlockOptions.Unbounded, TaskScheduler scheduler = null)
+        {
+            var options = new ExecutionDataflowBlockOptions
+            {
+                MaxDegreeOfParallelism = maxDegreeOfParallelism
+            };
+            if (scheduler != null)
+                options.TaskScheduler = scheduler;
+
+            var block = new ActionBlock<T>(body, options);
+
+            await foreach (var item in source)
+                block.Post(item);
+
+            block.Complete();
+            await block.Completion;
+        }
+    }
+}
